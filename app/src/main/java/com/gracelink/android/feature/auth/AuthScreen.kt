680eeea5154a6ac12545066201f0cdd3543b8da6
Z User <z@container>
2026-06-23 14:34:32 +0000

    fix: add missing Image import to AuthScreen and OnboardingScreen

--- app/src/main/java/com/gracelink/android/feature/auth/AuthScreen.kt
+++ app/src/main/java/com/gracelink/android/feature/auth/AuthScreen.kt
@@ -1,6 +1,7 @@
 package com.gracelink.android.feature.auth
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
